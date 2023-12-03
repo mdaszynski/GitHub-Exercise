a57eec7d0cab11e7bbc7e385cb4fa801b53d9ce0
Maciej Daszynski <maciej.t.daszynski@gmail.com>
2023-12-04 00:32:05 +0100

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Maciej Daszyński on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
