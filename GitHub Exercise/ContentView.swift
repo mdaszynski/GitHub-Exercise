99e8ff829202b10035c62b01eb81fc5368242dda
Maciej Daszynski <maciej.t.daszynski@gmail.com>
2023-12-05 00:12:19 +0100

    Version 3.0 - Blue

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-            Text("V. 2.0")
+            Text("V. 3.0")
                 .font(.title)
                 .fontWeight(.black)
             Rectangle()
         }
         .padding()
-        .foregroundColor(.red)
+        .foregroundColor(.blue)
     }
 }
 
